298d1ce76230b60b22e78eba1bb5408fac1d558c
Hayden50 <haydenrieder50@hotmail.com>
2021-06-30 21:24:18 -0400

    Set up Mikro-ORM

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,17 @@
-// Create instance of express object
+import {MikroORM} from "@mikro-orm/core";
+import microConfig from "./mikro-orm.config";
 const express = require('express');
-const app : object = express();
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+    console.log(orm.em);
+    // Create instance of express object
+    const app = express();
+    app.listen(3000, () => {
+        `Listening on port 3000...`
+    });
+}
+
+main().catch((e) => {
+    console.error(e);
+});
