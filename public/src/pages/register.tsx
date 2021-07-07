483472ee9b07308b9d48be885621925a874e7b23
Hayden50 <haydenrieder50@hotmail.com>
2021-07-07 16:29:34 -0400

    Fixed register screen

--- public/src/pages/register.tsx
+++ public/src/pages/register.tsx
@@ -1,29 +1,36 @@
 import React from 'react';
-import {Formik, Form} from "formik";
+import "formik";
+import {Form, Formik} from "formik";
 import {Wrapper} from "../components/Wrapper";
 import {InputField} from "../components/InputField";
 import {Box, Button, FormControl, FormLabel, Input} from "@chakra-ui/react";
 
-interface registerProps {}
+interface registerProps {
+}
 
-const Register : React.FC<registerProps> = ({}) => {
+const Register: React.FC<registerProps> = ({}) => {
     return (
         <Wrapper>
-            <Formik initialValues={{ email:"", password:""}} onSubmit={(values) => {console.log(values);}}>
-                {() => ({}) => (
+            <Formik initialValues={{email: "", password: ""}} onSubmit={(values) => {
+                        console.log(values);}}>
+                {({isSubmitting}) =>
                     <Form>
-                        <InputField name='email' label='Email'/>
+                        <InputField name='first name' label='First Name'/>
+                        <Box mt={4}>
+                            <InputField name='last name' label='Last Name'/>
+                        </Box>
+                        <Box mt={4}>
+                            <InputField name='email' label='Email'/>
+                        </Box>
                         <Box mt={4}>
                             <InputField name='password' label='Password'/>
                         </Box>
-                        <Button mt={4} type="submit">Submit</Button>
+                        <Box>
+                            <Button size="md" isLoading={isSubmitting} mt={4} type="submit" colorScheme="linkedin">Submit</Button>
+                        </Box>
                     </Form>
-                )}
+                }
             </Formik>
-            <FormControl id="first-name" isRequired>
-                <FormLabel>First name</FormLabel>
-                <Input placeholder="First name" />
-            </FormControl>
         </Wrapper>
     );
 }
