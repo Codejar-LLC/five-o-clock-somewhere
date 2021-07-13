153bf1238455e1fe0c9ecd3207290c675c8deef3
Hayden50 <haydenrieder50@hotmail.com>
2021-07-13 19:21:43 -0400

    Add me query

--- public/src/pages/index.tsx
+++ public/src/pages/index.tsx
@@ -1,3 +1,23 @@
-const Index = () => <div>Hello World</div>
+import React from "react";
+import {useMeQuery} from "../generated/graphql";
 
-export default Index
+const Index : React.FC<{}> = () => {
+    const {data, loading} = useMeQuery();
+
+    let response;
+    if (loading) {
+        response = null;
+    } else if (!data?.me) {
+        response = `You are not logged in :(`;
+    } else if (data.me) {
+        response = `Hello ${data.me.first_name}!`
+    }
+    console.log(data?.me);
+
+    return (
+
+        <div>{response}</div>
+    );
+}
+
+export default Index;
