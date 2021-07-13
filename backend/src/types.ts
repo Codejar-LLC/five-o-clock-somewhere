1ea013a0f2b5635be0ca21449ae2c9e3344864e1
Hayden50 <haydenrieder50@hotmail.com>
2021-07-13 19:21:18 -0400

    Fix TS error

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,10 +1,11 @@
 import {Connection, EntityManager, IDatabaseDriver} from "@mikro-orm/core";
 import {Request, Response} from "express";
+import {Session, SessionData} from "express-session";
 
 // User defined context that is accessed in Resolvers
 export type MyCtx = {
     em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-    req: Request & { // @ts-ignore
-        session : Express.Session };
+    req: Request & {
+        session : Session & Partial<SessionData> & { userId ?: number }};
     res: Response;
 };
